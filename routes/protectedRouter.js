db68e264d5a6e63c2eb857146155261596d437ef
samvdr_hi10 <stijnn.buys@gmail.com>
2021-04-28 19:46:27 +0200

    clients

--- routes/protectedRouter.js
+++ routes/protectedRouter.js
@@ -12,27 +12,48 @@ const opts = {
   ca: [fs.readFileSync("/home/stijn/keys/data/ss_cert_data.crt")],
 };
 
-router.get("/pdata1.json", (req, res) => {
+router.get("/pdata1.json", validateCertificateClientOne, (req, res) => {
+  res.send({ name: "bulbasaur", color: "green" });
+});
+
+router.get("/pdata2.json", validateCertificateClientTwo, (req, res) => {
+  res.send({ name: "charmander", color: "orange" });
+});
+
+function validateCertificateClientTwo(req, res, nex) {
   const cert = req.socket.getPeerCertificate();
-  if (req.client.authorized) {
-    res.send(
-      `Hello ${cert.subject.CN}, your certificate was issued by ${cert.issuer.CN}!`
-    );
+
+  if (req.client.authorized && cert.subject.CN == "client02.local") {
+    nex();
   } else if (cert.subject) {
     res
-      .status(403)
+      .sendStatus(403)
       .send(
-        `Sorry ${cert.subject.CN}, certificates from ${cert.issuer.CN} are not welcome here.`
+        "Sorry ${cert.subject.CN}, certificates from ${cert.issuer.CN} are not welcome here."
       );
   } else {
     res
       .status(401)
       .send(`Sorry, but you need to provide a client certificate to continue.`);
   }
-});
+}
 
-router.get("/pdata2.json", (req, res) => {
-  res.send("pdata2");
-});
+function validateCertificateClientOne(req, res, nex) {
+  const cert = req.socket.getPeerCertificate();
+
+  if (req.client.authorized && cert.subject.CN == "client01.local") {
+    nex();
+  } else if (cert.subject) {
+    res
+      .sendStatus(403)
+      .send(
+        "Sorry ${cert.subject.CN}, certificates from ${cert.issuer.CN} are not welcome here."
+      );
+  } else {
+    res
+      .status(401)
+      .send(`Sorry, but you need to provide a client certificate to continue.`);
+  }
+}
 
 module.exports = router;
